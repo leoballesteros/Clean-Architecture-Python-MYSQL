5521ec1079fb768efc71a074dbbee977f1e41745
unknown <leoballesteros1999@gmail.com>
2026-04-25 09:58:53 -0300

    connection.py

--- project/database/connection.py
+++ project/database/connection.py
@@ -1,25 +1,5 @@
-from __future__ import annotations
-
-from contextlib import contextmanager
-from typing import Iterator
-
 import mysql.connector
-from mysql.connector import MySQLConnection
+from config.settings import DB_CONFIG
 
-from project.config.settings import Settings
-
-
-@contextmanager
-def get_connection(settings: Settings) -> Iterator[MySQLConnection]:
-    connection = mysql.connector.connect(
-        host=settings.db_host,
-        port=settings.db_port,
-        user=settings.db_user,
-        password=settings.db_password,
-        database=settings.db_name,
-    )
-    try:
-        yield connection
-    finally:
-        connection.close()
-
+def get_connection():
+    return mysql.connector.connect(**DB_CONFIG)
